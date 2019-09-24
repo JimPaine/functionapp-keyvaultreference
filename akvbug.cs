dcb22de82316024a2a4aa7427a33124ec8098da6
Jim Paine <jim@jimpaine.io>
2019-09-24 15:21:46 +0100

    added mitigation eg.

--- akvbug.cs
+++ akvbug.cs
@@ -4,6 +4,8 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
 
 namespace functionapp_keyvaultreference
 {
@@ -14,10 +16,21 @@ namespace functionapp_keyvaultreference
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string result = Environment.GetEnvironmentVariable("bug");
+            string goDirect = req.Query["goDirect"];
+
+            string result = goDirect == "false" || string.IsNullOrWhiteSpace(goDirect)
+            ? Environment.GetEnvironmentVariable("bug")
+            : GetSecret();
+
             return !string.IsNullOrWhiteSpace(result) 
                 ? new OkObjectResult(result) 
                 : new NotFoundObjectResult("Environment variable is null or whitespace") as IActionResult;
+        }
+
+        private static string GetSecret()
+        {
+            KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
+            return keyVaultClient.GetSecretAsync("https://vault9486.vault.azure.net/secrets/bug/c198ab77b18d48f0b7f72cb51476899a").Result.Value;
         }
     }
 }
